e309ef5054c1a6dcfeb5001a5bf692068dc47b1d
Matthias Kadenbach <matthias.kadenbach@gmail.com>
2016-11-21 21:27:37 -0800

    fix wan ip lookup

--- home/bash/src/aliases/ip.darwin.sh
+++ home/bash/src/aliases/ip.darwin.sh
@@ -1,2 +1,4 @@
-alias wanip="curl http://echoip.com"
+alias wanip="curl https://v4.ifconfig.co"
+alias wanip4="curl https://v4.ifconfig.co"
+alias wanip6="curl https://v6.ifconfig.co"
 alias lanip="echo $(ipconfig getifaddr en0) $(ipconfig getifaddr en1)"
--- home/bash/src/aliases/ip.linux.sh
+++ home/bash/src/aliases/ip.linux.sh
@@ -1,4 +1,6 @@
-alias wanip="curl http://echoip.com"
+alias wanip="curl https://v4.ifconfig.co"
+alias wanip4="curl https://v4.ifconfig.co"
+alias wanip6="curl https://v6.ifconfig.co"
 
 # @todo
 # alias lanip=...
