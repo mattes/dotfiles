5a8685830e3fd3ac6cc2d105b1ef40cf634a9c78
Matthias Kadenbach <matthias.kadenbach@gmail.com>
2019-06-07 19:36:34 -0700

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 for path in $(cd home && find . -type f -not -path './bash/*'); do
   file=/home/$(whoami)/.${path:2}
   dir=$(dirname $file)
-  relative=$(realpath --relative-to $(realpath /home/$(whoami)/dotfiles/$path) /home/$(whoami)/dotfiles)
+  relative=$(realpath --relative-to $(realpath /home/$(whoami)/dotfiles/home/$path) /home/$(whoami)/dotfiles/home)
   symlink=${relative:0:-2}dotfiles/${path:2}
 
   if [[ ! -e "$dir" ]]; then
