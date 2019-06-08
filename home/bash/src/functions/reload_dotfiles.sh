03fc8886d5a75dd478be889ca451fb121e9ce106
Matthias Kadenbach <matthias.kadenbach@gmail.com>
2019-06-08 01:29:02 -0700

    fix reload_dotfiles

--- home/bash/src/functions/reload_dotfiles.sh
+++ home/bash/src/functions/reload_dotfiles.sh
@@ -1,4 +1,4 @@
 function reload_dotfiles {
-  (cd $HOME/dotfiles/bash && . build.sh)
-  source $HOME/dotfiles/bash/cached.sh
+  (cd $HOME/dotfiles/home/bash && . build.sh)
+  source $HOME/dotfiles/home/bash/cached.sh
 }
